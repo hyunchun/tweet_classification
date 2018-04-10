0bc9d72596993fa6d8d9a86b9663c2321391c444
hyunchun <hyunchun@umich.edu>
2018-04-10 14:52:04 -0400

    modified README

--- classifiers/classification_dynet.py
+++ classifiers/classification_dynet.py
@@ -14,25 +14,6 @@ try:
 except ImportError:
     import simplejson as json
 
-from sklearn.svm import SVC, LinearSVC
-from sklearn.svm import LinearSVC
-from sklearn.model_selection import StratifiedKFold, GridSearchCV
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import roc_curve
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_score
-from sklearn import metrics
-
-from sklearn.multiclass import OneVsOneClassifier
-from sklearn.multiclass import OneVsRestClassifier
-
-from sklearn.utils import resample
-
-from sklearn import metrics
-
-
 def extract_from_json(inFile):
     textSet = []
     contentLabelSet = []
@@ -63,7 +44,6 @@ def label_separator(label_to_separate, text_set, content_label_set, type_label_s
         count = 0
         for line in text_set:
             if int(type_label_set[count]) == 0:
-                #print(text_set[count])
                 majority.append(line)
                 majority_content_label.append(content_label_set[count])
                 majority_type_label.append(type_label_set[count])
@@ -72,7 +52,7 @@ def label_separator(label_to_separate, text_set, content_label_set, type_label_s
                 minority_content_label.append(content_label_set[count])
                 minority_type_label.append(type_label_set[count])
             count += 1
-    #majority_n = np.concatenate(majority)
+    
     print(len(text_set))
     print(len(majority))
     print(len(minority))
@@ -81,10 +61,7 @@ def label_separator(label_to_separate, text_set, content_label_set, type_label_s
 def extract_dictionary(dataset, word_dict):
     index = 0
     for line in dataset:
-        # line = line.split()
-        #print(line)
         for word in line:
-            #print(word)
             # just word itself 
             if word not in word_dict:
                 word_dict[word] = index
@@ -119,7 +96,6 @@ def generate_feature_matrix(dataset, word_dict):
     train_dict = {}
     line_count = 0
     for line in dataset:
-        # line = line.split()
         for word in line:
             # check if RT
             if word == "RT":
@@ -160,13 +136,15 @@ def generate_feature_matrix(dataset, word_dict):
 
 # ----------------------------------------------------------
 def main():
-    #try:
-    #    dy.cg_revert()
-    #except:
     dy.renew_cg()
+    
+    try:
+        train_file = open("%s" %(sys.argv[1]))
+        test_file = open("%s" %(sys.argv[2]))
+    except:
+        print("python classification_dynet.py <train_file> <test_file>")
+        sys.exit(1)
 
-    train_file = open("%s" %(sys.argv[1]))
-    test_file = open("%s" %(sys.argv[2]))
     train_text_set, train_content_label_set, train_type_label_set, unique_content, unique_type = extract_from_json(train_file)
     test_text_set, test_content_label_set, test_type_label_set, _, _ = extract_from_json(test_file)
     
@@ -194,22 +172,16 @@ def main():
     lookup = para_collec.add_lookup_parameters((features_total, 200), dy.NormalInitializer())
 
     trainer = dy.SimpleSGDTrainer(para_collec)
-    #print(X_train)
     
     for i in range(0, 1):
         # resample minority and majority classes
         majority, majority_content_label, majority_type_label, minority, minority_content_label, minority_type_label = label_separator("type", train_feature_matrix, train_content_label_set, train_type_label_set)
         minority_u_text, minority_u_content_label, minority_u_type_label = resample(minority, minority_content_label, minority_type_label, replace=True, n_samples=int(len(majority) * 3), random_state=123)
 
-        #X_train = majority + minority_u_text
-        #y_train_content = majority_content_label + minority_u_content_label
-        #y_train_type = majority_type_label + minority_u_type_label
-        
         X_train = train_feature_matrix
         y_train_content = train_content_label_set
         y_train_type = train_type_label_set
 
-        #for index in range(0, len(X_train)):
         for index in range(0, 500):
 
             w1 = dy.parameter(pW1)
@@ -224,16 +196,13 @@ def main():
             bias3_type = dy.parameter(pBias3_type)
             
             input_text = []
-            #line = train_text_set[index]
             input_array = X_train[index]
-            #print(X_train[index].size)
+            
             for i in range(0, X_train[index].size):
                 if X_train[index][i] > 0:
                     input_text.append(lookup[X_train[index][i]])
 
             x = dy.concatenate(input_text, 1)
-            #print(x.npvalue().shape)
-            #print(dy.sum_dim(x, [1]).npvalue().shape)
             e_in = dy.sum_dim(x, [1])/features_total
             e_affin1 = dy.affine_transform([bias1, w1, e_in])
             e_affin1 = dy.rectify(e_affin1)
@@ -280,7 +249,6 @@ def main():
     bias3_type = dy.parameter(pBias3_type)
 
     for index in range(0, len(test_feature_matrix)):
-        # x = dy.inputTensor(test_feature_matrix[index])
        
         input_text = []
         line = train_text_set[index]
@@ -330,7 +298,6 @@ def main():
     misclassification_content = 0
     misclassification_type = 0
     for index in range(0, len(pred_content)):
-        #print(pred_content[index], test_content_label_set[index])
         if pred_content[index] != test_content_label_set[index]:
             misclassification_content += 1
         if pred_type[index] != test_type_label_set[index]:
